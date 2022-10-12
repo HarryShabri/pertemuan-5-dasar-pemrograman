383b3c9632531bd20ab1ae394fb3deb701894a3d
Harry Shabri <115128258+HarryShabri@users.noreply.github.com>
2022-10-12 16:02:13 +0700

    Add files via upload

---
+++ Latihan.py
@@ -0,0 +1,7 @@
+for i in range(2):
+    print ("data Ke - " + str(i+1))
+    nama=input("Masukkan Nim anda : ")
+    uts=int(input("Masukkan Nilai UTS anda :"))
+    uas=int(input("Masukkan Nilai UAS : "))
+    print("NIm anda adalah %s nilai UTS anda %i nilai UTS anda %i" % (nama,uts,uas))
+    print("-------------------------------------\n")
